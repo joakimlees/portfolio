1665643bdc944f11a3e98530d2ba9cc43d6df740
joakimlees <joakimlee_sletten@outlook.com>
2024-03-18 05:02:07 +0100

    working on navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,34 @@
-import styled from "styled-components";
+import * as Styled from "./styles/index";
+import { Link } from "react-router-dom";
 
-export const Nav = styled.nav``;
+export function Nav() {
+  return (
+    <Styled.Navigation>
+      <div>
+        <Link to="">Logo</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="">About</Link>
+        </li>
+        <li>
+          <Link to="">Projects</Link>
+        </li>
+      </ul>
+      <ul className="social-links">
+        <li>
+          <Link to="">GH</Link>
+        </li>
+        <li>
+          <Link to="">In</Link>
+        </li>
+        <li>
+          <Link to="">IG</Link>
+        </li>
+        <li>
+          <Link to="">FB</Link>
+        </li>
+      </ul>
+    </Styled.Navigation>
+  );
+}
--- src/components/styles/index.ts
+++ src/components/styles/index.ts
@@ -1,2 +1,3 @@
 export * from "./theme";
 export * from "./Layout.Styles";
+export * from "./Navigation.Styles";

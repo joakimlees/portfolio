2ad29a5e0e5ef0a610b1490ebd5b960ec244e3d5
joakimlees <joakimlee_sletten@outlook.com>
2024-03-21 22:54:44 +0100

    styling navigation

--- src/components/styles/Navigation.Styles.ts
+++ src/components/styles/Navigation.Styles.ts
@@ -1,9 +1,26 @@
 import styled from "styled-components";
 
 export const Navigation = styled.nav`
-  border-style: solid;
+  background-color: ${({ theme }) => theme.color.darkPurple};
   position: fixed;
   right: 0px;
   top: 0px;
   height: 100vh;
+  width: min(75vw, 375px);
+  box-shadow: -10px 0 25px -15px ${({ theme }) => theme.color.dark};
+
+  ul {
+    list-style: none;
+    text-decoration: none;
+
+    li {
+      a {
+        font-size: ${({ theme }) => theme.font.size.small};
+        font-family: ${({ theme }) => theme.font.family.navLink};
+        font-weight: ${({ theme }) => theme.font.weight.semiBold};
+        color: ${({ theme }) => theme.color.light};
+        text-decoration: none;
+      }
+    }
+  }
 `;

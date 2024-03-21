fbc98e6247d60e93cfbdf081c76ddba2c045a1a9
joakimlees <joakimlee_sletten@outlook.com>
2024-03-21 23:30:38 +0100

    changes on nav

--- src/components/styles/Navigation.Styles.ts
+++ src/components/styles/Navigation.Styles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Navigation = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: ${({ theme }) => theme.color.darkPurple};
   position: fixed;
   right: 0px;

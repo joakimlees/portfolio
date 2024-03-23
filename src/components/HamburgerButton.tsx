c32e0287c79b6fa975541d55546a13c41e59ed7b
joakimlees <joakimlee_sletten@outlook.com>
2024-03-23 13:17:46 +0100

    styling burger menu

--- src/components/HamburgerButton.tsx
+++ src/components/HamburgerButton.tsx
@@ -4,9 +4,9 @@ export function HamburgerButton() {
   return (
     <Styled.HamburgerButton>
       <div>
-        <span></span>
-        <span></span>
-        <span></span>
+        <span className="lineOne"></span>
+        <span className="lineTwo"></span>
+        <span className="lineThree"></span>
       </div>
     </Styled.HamburgerButton>
   );
--- src/components/styles/HamburgerButton.Styles.ts
+++ src/components/styles/HamburgerButton.Styles.ts
@@ -3,13 +3,48 @@ import styled from "styled-components";
 export const HamburgerButton = styled.button`
   display: block;
   position: fixed;
-  top: 15px;
+  top: 20px;
+  border: none;
   right: 50px;
-  border: 1px solid white;
-  height: 40px;
-  width: 40px;
+  height: 30px;
+  width: 50px;
   background: none;
   padding: 0px;
   margin: 0px;
   z-index: 10;
+
+  div {
+    display: flex;
+    margin: auto;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+
+    span {
+      position: absolute;
+      display: block;
+      width: 100%;
+      height: 0px;
+      border: 2px solid white;
+      border-radius: 10px;
+    }
+
+    .lineOne {
+      top: 0;
+      left: 0;
+      width: 70%;
+    }
+
+    .lineTwo {
+      width: 70%;
+      left: 15%;
+    }
+
+    .lineThree {
+      bottom: 0;
+      right: 0;
+      width: 70%;
+    }
+  }
 `;

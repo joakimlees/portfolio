431d9d8bd5f0f1e59e7f6db059127136ffa1e822
joakimlees <joakimlee_sletten@outlook.com>
2025-07-28 00:06:41 +0200

    started on hero-section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,7 +3,19 @@ import * as Styled from "../styles/index";
 export function Hero() {
   return (
     <Styled.Hero>
-      <h1>My hero section 123</h1>
+      <div className="hero-main-content">
+        <span>FRONTEND DEVELOPER</span>
+        <h1>Joakim Lee</h1>
+        <p>
+          Hello there, and welcome to my little corner of the internet. I’m a
+          front-end developer from Norway, and this is where I showcase my
+          projects and share a bit about who I am. Have a look around
+        </p>
+        <div className="duo-button-container">
+          <a href="">View my projects</a>
+          <a href="">Contact me</a>
+        </div>
+      </div>
     </Styled.Hero>
   );
 }

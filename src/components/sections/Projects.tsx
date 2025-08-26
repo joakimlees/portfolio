d389b25c52956fa153353e940b3a45385c3d3c34
joakimlees <joakimlee_sletten@outlook.com>
2025-08-26 23:42:19 +0200

    started on project cards

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,9 +1,50 @@
 import * as Styled from "../styles/index";
+import {
+  IconReact,
+  IconTypeScript,
+  IconTailwind,
+  IconBootstrap,
+  IconNpm,
+  IconNodeJs,
+  IconStyledComponents,
+  IconScss,
+} from "../icons";
+
+import Image from "../../build/images/sciencemus.jpg";
 
 export function Projects() {
   return (
     <Styled.Projects>
       <h2>Projects</h2>
+      <div className="projects__container">
+        <div className="project__card">
+          <div className="project__card__image">
+            <img src={Image} alt="" />
+          </div>
+          <div className="project__card__content">
+            <h3>Science Museum</h3>
+            <p className="project__card__text">
+              Qui magnam praesentium sit accusamus illo et magnam veniam et
+              internos blanditiis. Est rerum debitis qui voluptatum quae vel
+              autem libero qui consequatur consequatur.
+            </p>
+            <div className="project__card__pills">
+              <div className="project__card__pill">
+                <IconTypeScript size={20} />
+                <p>TypeScript</p>
+              </div>
+              <div className="project__card__pill">
+                <IconTypeScript size={20} />
+                <p>TypeScript</p>
+              </div>
+              <div className="project__card__pill">
+                <IconTypeScript size={20} />
+                <p>TypeScript</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Styled.Projects>
   );
 }
--- src/components/styles/sections/Projects.Styles.ts
+++ src/components/styles/sections/Projects.Styles.ts
@@ -13,4 +13,63 @@ export const Projects = styled.section`
     border-color: ${({ theme }) => theme.color.primaryDark};
     padding-bottom: 2px;
   }
+
+  .projects__container {
+    display: flex;
+    gap: 30px;
+
+    .project__card {
+      display: flex;
+      padding: 15px;
+      gap: 20px;
+      width: 800px;
+      border: solid;
+
+      .project__card__image {
+        width: 300px;
+        height: 100%;
+        overflow: hidden;
+        border-radius: 5px;
+        border-style: solid;
+        border-width: 1px;
+
+        img {
+          display: block;
+          width: 100%;
+          height: 100%;
+
+          object-fit: cover;
+          object-position: center;
+        }
+      }
+
+      .project__card__content {
+        h3 {
+          font-family: ${({ theme }) => theme.font.family.heading};
+          font-weight: ${({ theme }) => theme.font.weight.semiBold};
+          color: ${({ theme }) => theme.color.secondaryGray};
+        }
+
+        .project__card__text {
+          margin: 25px 0px;
+        }
+
+        .project__card__pills {
+          display: flex;
+          gap: 10px;
+
+          .project__card__pill {
+            font-size: ${({ theme }) => theme.font.size.xxSmall};
+            display: flex;
+            align-items: center;
+            gap: 5px;
+            border-style: solid;
+            border-width: 1px;
+            border-radius: 5px;
+            padding: 5px 20px 5px 5px;
+          }
+        }
+      }
+    }
+  }
 `;

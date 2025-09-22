4339cc93b1a519dfeea7bf3e9514b38830885de1
joakimlees <joakimlee_sletten@outlook.com>
2025-09-22 20:40:34 +0200

    footer design

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -5,18 +5,48 @@ export function Footer() {
   return (
     <FadeInOnScroll>
       <Styled.Footer>
-        <div>Footer</div>
-        <p>
-          Icons by Font Awesome (CC BY 4.0) Icons by Font Awesome (CC BY 4.0)
-          Font Awesome Free v7.0.0 by @fontawesome License:
-          https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.
-        </p>
-        <p>
-          Icons for GitHub and styled-components are used under the MIT License.
-          <br />
-          All other technology logos are used under their official brand or
-          public resources.
-        </p>
+        <Styled.CSContainer>
+          <div className="footer-split-container">
+            <div className="footer-left">
+              <p>&copy; 2025 joakimlees.com</p>
+
+              <p>
+                <small>
+                  Icons by Font Awesome (CC BY 4.0) · Font Awesome Free v7.0.0
+                  by Font Awesome · License:{" "}
+                  <a
+                    href="https://fontawesome.com/license/free"
+                    rel="noopener noreferrer"
+                  >
+                    fontawesome.com/license/free
+                  </a>{" "}
+                  · Copyright 2025 Fonticons, Inc.
+                </small>
+              </p>
+
+              <p>
+                <small>
+                  Icons for GitHub and styled-components are used under the MIT
+                  License.
+                </small>
+              </p>
+
+              <p>
+                <small>
+                  All other technology logos are used under their official brand
+                  or public resources.
+                </small>
+              </p>
+            </div>
+            <div className="footer-right">
+              <p>
+                Initially crafted in Figma and brought to life in Visual Studio
+                Code. Built with React, Vite & TypeScript, styled with CSS
+                Modules. Deployed with x
+              </p>
+            </div>
+          </div>
+        </Styled.CSContainer>
       </Styled.Footer>
     </FadeInOnScroll>
   );
--- src/components/styles/sections/Footer.Styles.ts
+++ src/components/styles/sections/Footer.Styles.ts
@@ -1,6 +1,33 @@
 import styled from "styled-components";
 
 export const Footer = styled.footer`
-  border: 1px solid red;
-  padding: 50px;
+  .footer-split-container {
+    display: flex;
+    border-top: solid;
+    border-width: 1px;
+    border-color: ${({ theme }) => theme.color.primaryDark};
+    padding: 80px 0px;
+    justify-content: space-between;
+    gap: 28px;
+    font-size: font-size: ${({ theme }) => theme.font.size.xxSmall};
+
+    .footer-right {
+      text-align: right;
+
+      span {
+      color: ${({ theme }) => theme.color.accentPink};
+
+      }
+
+
+    }
+
+    p {
+      margin-top: 12px;
+    }
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.color.accentPink};
+    }
+  }
 `;
